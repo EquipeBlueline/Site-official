6f50733c4ff7723539984cf450a75943b93af63b
FELIPE ALVES RODRIGUES <00001106743088sp@aluno.educacao.sp.gov.br>
2025-09-09 14:31:31 -0300

    começando do zero

--- script.js
+++
@@ -1,9 +0,0 @@
-
-function mostrarSecao(secao) {
-    // esconde todas
-    document.querySelectorAll('.secao').forEach(s => s.classList.remove('active'));
-    // mostra só a clicada
-    const ativa = document.getElementById(secao);
-    if (ativa) ativa.classList.add('active');
-}
-
