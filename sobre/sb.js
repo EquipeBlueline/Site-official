f8125c13a0e77f2163d15778dad38067d17d3378
Felipe <felipealvesrodrigues421@gmail.com>
2025-09-11 15:50:11 -0300

    Arrumando menu na pagina de sobre

--- sobre/sb.js
+++ sobre/sb.js
@@ -1,3 +1,12 @@
+const toggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("voltar");
+
+toggle.addEventListener("click", () => {
+    menu.classList.toggle("show");
+});
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
     window.mostrarCard = function(cardId) {
         const cards = document.querySelectorAll('.card');
